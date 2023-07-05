8360a40df76049454889eec8af681698d1cb6b76
Tarun2799 <131796732+Tarun2799@users.noreply.github.com>
2023-07-05 17:19:31 +0530

    Add files via upload
    THAT'S IT. Next, I'll update more.

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drane',
+        productNumber: '54123',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '74923',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ruko F11 Proo Drone',
+        productNumber: '54853',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: ' Drone with CAmera Drone',
+        productNumber: '97622',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '79513',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '85123',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00853',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
